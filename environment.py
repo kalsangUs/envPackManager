4c4dbb26ae34226910c0fe3a6371628d7781ba55
Kalsang <kalwrkng@gmail.com>
2025-09-04 13:16:42 +0545

    Wrote some utility functions

--- environment.py
+++ environment.py
@@ -1,10 +1,14 @@
+import subprocess
+import os
+from pathlib import Path
+from typing import List
 
 
 class Environment():
-    def __init__(self, name, path, package_manager, packageType):
+    def __init__(self, name, path, packageType, packageManager=None):
         self.__name = name
         self.__path = path
-        self.__package_manager = package_manager
+        self.__packageManager = packageManager
         self.__packages = []
         self.__packageType = packageType
 
@@ -13,6 +17,9 @@ class Environment():
 
     def getPath(self):
         return self.__path
+
+    def __repr__(self):
+        return f"Environment(name={self.__name}, path={self.__path}, packageType={self.__packageType})"
 
 
 class PackageManager():
@@ -26,12 +33,23 @@ class PackageManager():
 class EnvironmentManager():
     def __init__(self):
         self.__environments = []
+        self.__environmentReader = None
+
+    def addEnvironmentReader(self, reader):
+        self.__environmentReader = reader
+
+    def loadEnvironments(self):
+        results = self.__environments.read_conda_environments()
+        for i in results:
+            name, path = i.split()[:2]
+            env = Environment(name, path, 'conda')
+            self.__environments.append(env)
 
     def addEnvironment(self, environment):
         pass
 
     def getEnvironments(self):
-        pass
+        return self.__environments
 
     def removeEnvironment(self, environment):
         pass
@@ -54,9 +72,19 @@ class EnvironmentReader:
         pass
 
     def read_conda_environments(self):
-        # code to list all conda envs
-        return []
+        environments = []
+        result = subprocess.run(
+            ['conda', 'env', 'list'],
+            capture_output=True, text=True
+        ).stdout
+        return result.splitlines()[3:-1]
 
-    def read_virtualenvs(self, path):
-        # code to list all virtualenvs in a path
-        return []
+
+if __name__ == "__main__":
+    reader = EnvironmentReader()
+
+    # Find venv environments in current directory
+    envs = reader.find_virtual_environments("~")
+    print("Found venv virtual environments:")
+    for env in envs:
+        print(env)
